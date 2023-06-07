548608d7a9a5b9ae6b9061d75226dc1d627ba84c
Assad Anabosi <assad.anabosi@icloud.com>
2023-06-07 18:07:28 +0300

    fix(comparison): log type by type

--- index.mjs
+++ index.mjs
@@ -2,13 +2,12 @@ import * as generate from "./array.generate.mjs";
 import compareAlgorithms from "./compare.mjs";
 
 let arraySizes = [10, 100, 1000, 10000, 100000];
+let types = ["Sorted", "Random", "Reversed"];
 
-arraySizes.forEach((size) => {
-    let sortedArray = generate.sortedArray(size);
-    let randomArray = generate.randomArray(size);
-    let reversedArray = generate.reversedArray(size);
-    // @desc  Compare the execution time of different sorting algorithms
-    compareAlgorithms("Sorted", sortedArray);
-    compareAlgorithms("Random", randomArray);
-    compareAlgorithms("Reversed", reversedArray);
+types.forEach((type) => {
+    arraySizes.forEach((size) => {
+        let array = generate[type.toLowerCase() + "Array"](size);
+        // @desc  Compare the execution time of different sorting algorithms
+        compareAlgorithms(type, array);
+    });
 });
