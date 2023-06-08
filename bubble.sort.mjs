880de144d5b63855bb08245235a3880ef19b5bec
Assad Anabosi <assad.anabosi@icloud.com>
2023-06-08 17:09:04 +0300

    fix(bubble): enhanced best-case O(n)

--- bubble.sort.mjs
+++ bubble.sort.mjs
@@ -1,15 +1,26 @@
 const bubbleSort = (array) => {
     let len = array.length;
+    let swapped;
+
     for (let i = 0; i < len - 1; i++) {
+        swapped = false;
+
         for (let j = 0; j < len - 1 - i; j++) {
             if (array[j] > array[j + 1]) {
                 // Swap elements
                 let temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                swapped = true;
             }
         }
+
+        // If no swaps were made in the inner loop, the array is already sorted
+        if (!swapped) {
+            break;
+        }
     }
+
     return array;
 }
 
