914318278f370ea7c7788eb9ccb1626be65a2f19
Assad Anabosi <assad.anabosi@icloud.com>
2023-06-09 00:28:19 +0300

    feat(merge): in-place merge sort

--- merge.sort.mjs
+++ merge.sort.mjs
@@ -36,4 +36,57 @@ const merge = (left, right) => {
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
+const mergeSortInPlace = (array) => {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    // Divide the array into two halves
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0, mid);
+    let right = array.slice(mid);
+  
+    // Recursively sort the two halves
+    mergeSort(left);
+    mergeSort(right);
+  
+    // Merge the sorted halves in-place
+    mergeInPlace(array, left, right);
+  
+    return array;
+  };
+
+const mergeInPlace = (array, left, right) => {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let arrayIndex = 0;
+  
+    // Compare elements from both arrays and merge them in sorted order
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        array[arrayIndex] = left[leftIndex];
+        leftIndex++;
+      } else {
+        array[arrayIndex] = right[rightIndex];
+        rightIndex++;
+      }
+      arrayIndex++;
+    }
+  
+    // Copy any remaining elements from the left array
+    while (leftIndex < left.length) {
+      array[arrayIndex] = left[leftIndex];
+      leftIndex++;
+      arrayIndex++;
+    }
+  
+    // Copy any remaining elements from the right array
+    while (rightIndex < right.length) {
+      array[arrayIndex] = right[rightIndex];
+      rightIndex++;
+      arrayIndex++;
+    }
+  };
+
 export default mergeSort;
+export { mergeSortInPlace };
